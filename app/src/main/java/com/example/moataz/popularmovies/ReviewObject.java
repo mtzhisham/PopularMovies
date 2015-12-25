67daf9c110b33f53526a1fb076d890b0a7632720
mtzhisham <mtzhisham@gmail.com>
2015-12-26 00:10:05 +0200

    extracting reviews and trailers from movies object class

--- app/src/main/java/com/example/moataz/popularmovies/ReviewObject.java
+++ app/src/main/java/com/example/moataz/popularmovies/ReviewObject.java
@@ -4,4 +4,16 @@ package com.example.moataz.popularmovies;
  * Created by Moataz on 12/25/2015.
  */
 public class ReviewObject {
+
+    String review;
+    String author;
+    int id;
+
+
+    ReviewObject(int id,String author, String review){
+        this.author=author;
+        this.review=review;
+        this.id=id;
+    }
+
 }
--- app/src/main/java/com/example/moataz/popularmovies/TrailerObject.java
+++ app/src/main/java/com/example/moataz/popularmovies/TrailerObject.java
@@ -1,71 +1,39 @@
 package com.example.moataz.popularmovies;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by Moataz on 12/25/2015.
  */
-public class TrailerObject implements Parcelable {
+public class TrailerObject  {
 
 
 
     String name;
+
+    public String getKey(int id) {
+
+        if(this.id == id) return key;
+
+        else
+        return null;
+    }
+
     String key;
+    int id;
+
 
     public TrailerObject(){
 
     }
 
-    public TrailerObject(int movieId, String movieTitle, String movieOverview, String moviePosterPath, double movieRating, String movieReleaseDate){
+    public TrailerObject(int id, String name, String key){
 
-        this.id = movieId;
-        this.title = movieTitle;
-        this.overview = movieOverview;
-        this.posterPath="http://image.tmdb.org/t/p/w342/"+moviePosterPath;
-        this.rating=movieRating;
-        this.releaseDate = movieReleaseDate;
-
-    }
-    private TrailerObject(Parcel in){
-        id=in.readInt();
-        title=in.readString();
-        overview= in.readString();
-        posterPath = in.readString();
-        rating = in.readDouble();
-        releaseDate = in.readString();
-
+        this.key = key;
+        this.name = name;
+        this.id=id;
 
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(id);
-        dest.writeString(title);
-        dest.writeString(overview);
-        dest.writeString(posterPath);
-        dest.writeDouble(rating);
-        dest.writeString(releaseDate);
-
-    }
-
-    public final Parcelable.Creator<MovieObject> CREATOR = new Parcelable.Creator<MovieObject>() {
-        @Override
-        public MovieObject createFromParcel(Parcel parcel) {
-            return new MovieObject(parcel);
-        }
-
-        @Override
-        public MovieObject[] newArray(int i) {
-            return new MovieObject[i];
-        }
-
-    };
 
 
 }
